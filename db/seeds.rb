f250d5cbb11efb0bd1d0f8d505f4ca5b47dcaf7c
root <root@BravoBase.localdomain>
2022-10-23 14:29:31 -0500

    add my favorite movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Rogue One:A Star Wars Story', :rating => 'PG-13',
+      :release_date => '16-Dec-2016'}
 ]
 
 more_movies.each do |movie|
